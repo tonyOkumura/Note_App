ab8c6f1787c64d790e5cf496c516b093150b8771
tonyOkumura <anton.tereshchenko.0003@bk.ru>
2023-07-14 10:18:47 +0300

    start

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/src/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: HomeScreen());
+  }
+}
